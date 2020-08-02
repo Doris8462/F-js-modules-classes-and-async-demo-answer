a3255612a6017afbba07ae53aee110eb68f631ac
kevindongzg <dongkevin94@gmail.com>
2020-08-02 15:48:37 +0800

    add test framework

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,6 +1,6 @@
-import obj from './module1.js'
-import { fn1, fn2 } from './module2.js'
+import obj from "./module1";
+import { fn1, fn2 } from "./module2";
 
-console.log(obj)
-fn1()
-fn2()
+console.log(obj);
+fn1();
+fn2();
